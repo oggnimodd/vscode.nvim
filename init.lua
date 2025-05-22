0a2316fc75148ff2da0b63d6b1c5c5512279dded
oggnimodd <arnold.ds.dev@gmail.com>
2025-05-22 17:44:57 +0700

    feat: Enable volar and typescript vue plugin for vue development

--- init.lua
+++ init.lua
@@ -662,6 +662,25 @@ require('lazy').setup({
         },
         svelte = {},
       }
+      local vue_language_server_path = vim.fn.expand '$MASON/packages' .. '/vue-language-server' .. '/node_modules/@vue/language-server'
+
+      local lspconfig = require 'lspconfig'
+
+      lspconfig.ts_ls.setup {
+        init_options = {
+          plugins = {
+            {
+              name = '@vue/typescript-plugin',
+              location = vue_language_server_path,
+              languages = { 'vue' },
+            },
+          },
+        },
+        filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+      }
+
+      -- No need to set `hybridMode` to `true` as it's the default value
+      lspconfig.volar.setup {}
 
       -- Ensure the servers and tools above are installed
       --
