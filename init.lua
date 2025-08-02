e5d721da519c2a1962ef68e90d9e6a7214b52ab4
oggnimodd <arnold.ds.dev@gmail.com>
2025-08-02 10:38:10 +0700

    feat: Add flash.nvim plugin and configuration

--- init.lua
+++ init.lua
@@ -592,6 +592,9 @@ require('lazy').setup({
   --  LSP Plugins
   require 'kickstart.plugins.lsp',
 
+  -- Flash Plugin
+  require 'kickstart.plugins.flash',
+
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
